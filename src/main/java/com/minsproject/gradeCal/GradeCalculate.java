4a8b8941e0054ce53a5ccf132f8513d1e0aab98e
min <www5981@naver.com>
2025-02-15 17:28:19 +0900

    일급 컬렉션으로 학점 계산 처리

--- src/main/java/com/minsproject/gradeCal/GradeCalculate.java
+++ src/main/java/com/minsproject/gradeCal/GradeCalculate.java
@@ -3,27 +3,19 @@ package com.minsproject.gradeCal;
 import java.util.List;
 
 public class GradeCalculate {
-    private final List<Course> courses;
+    private final Courses courses;
 
     public GradeCalculate(List<Course> courses) {
-        this.courses = courses;
+        this.courses = new Courses(courses);
     }
 
-    /*
-    요구사항
-    평균학점 계산 방법 = (학점수*교과목 평점)의 합계 / 수강신청 총 학점 수
-    일급 컬렉션 사용
-     */
     public double calculateGrade() {
         // (학점수*교과목 평점)의 합계
-        double mulitpliedCreditAndCourseGrade = 0;
-        for (Course course : courses) {
-            mulitpliedCreditAndCourseGrade += course.multiplyCreditAndCourseGrade();
-        }
+        double totalMultipliedCreditAndCourseGrade = courses.multiplyCreditAndCourseGrade();
 
         // 수강신청 총학점 수
-        int totalCompletedCredit = courses.stream().mapToInt(Course::getCredit).sum();
+        int totalCompletedCredit = courses.calculateTotalCompletedCredit();
 
-        return mulitpliedCreditAndCourseGrade / totalCompletedCredit;
+        return totalMultipliedCreditAndCourseGrade / totalCompletedCredit;
     }
 }
